dca87cb834d51231eaed7be9e003ccb21a168ebf
Andrew Liebchen <liebchen@fb.com>
2017-04-18 20:52:58 -0700

    GOT IT WORKING

--- app/index.js
+++ app/index.js
@@ -1,12 +1,21 @@
 var Generator = require('yeoman-generator');
+var ncp = require('ncp').ncp;
 
 module.exports = class extends Generator {
-  writing: {
-    app: function () {
-      this.fs.copyTpl(
-        this.templatePath('*/*'),
-        this.destinationPath('*/*'),
-      );
-    },
+  constructor(args, opts) {
+    super(args, opts);
+    this.argument('appname', { type: String, required: true });
+    this.log(this.options.appname);
+  }
+
+  writing() {
+    ncp(
+      this.templatePath(),
+      this.destinationPath(this.options.appname + '.framer')
+    );
+  }
+
+  end() {
+    this.log('Framer project created. Have fun!');
   }
 };
